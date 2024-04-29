a6333df9105a1f123904e3eaa25354bb6af76e72
Deivid <deivid.cardoso@weduka.com.br>
2024-04-29 19:05:21 -0300

    fix: migration

--- api/prisma/migrations/20240429220417_new_entities/migration.sql
+++ api/prisma/migrations/20240429220417_new_entities/migration.sql
@@ -1,12 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-DROP TABLE "User";
-
 -- CreateTable
 CREATE TABLE "users" (
     "id" SERIAL NOT NULL,
@@ -33,25 +24,29 @@ CREATE TABLE "events" (
     "showUsers" BOOLEAN NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" INTEGER NOT NULL,
 
     CONSTRAINT "events_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "userEvents" (
+CREATE TABLE "eventUsers" (
     "id" SERIAL NOT NULL,
     "userId" INTEGER,
     "eventId" INTEGER,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    CONSTRAINT "userEvents_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "eventUsers_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
 
 -- AddForeignKey
-ALTER TABLE "userEvents" ADD CONSTRAINT "userEvents_eventId_fkey" FOREIGN KEY ("eventId") REFERENCES "events"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "events" ADD CONSTRAINT "events_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "userEvents" ADD CONSTRAINT "userEvents_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "eventUsers" ADD CONSTRAINT "eventUsers_eventId_fkey" FOREIGN KEY ("eventId") REFERENCES "events"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "eventUsers" ADD CONSTRAINT "eventUsers_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
