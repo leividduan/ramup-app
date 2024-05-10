02829f99810c1965a4911fe1ed43c06da9e6d30c
Deivid <deivid.cardoso@weduka.com.br>
2024-05-10 10:19:21 -0300

    Fix: some changes

--- api/src/services/generateAccessToken.ts
+++ api/src/services/generateAccessToken.ts
@@ -2,5 +2,5 @@ import { User } from '@prisma/client';
 import jwt from 'jsonwebtoken';
 
 export function generateAccessToken(user: User) {
-  return jwt.sign({id: user.id, name: user.name, email: user.email}, process.env.TOKEN_SECRET as string, {expiresIn: '8h'});
+  return jwt.sign({ id: user.id, name: user.name, email: user.email }, process.env.TOKEN_SECRET as string, { expiresIn: '8h' });
 }
